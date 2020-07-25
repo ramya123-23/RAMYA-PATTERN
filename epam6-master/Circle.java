256cd101470547d00fe9e12d6f0b62fd103a2a07
ramya123-23 <63300077+ramya123-23@users.noreply.github.com>
2020-07-25 15:02:42 +0530

    Add files via upload

---
+++ epam6-master/Circle.java
@@ -0,0 +1,9 @@
+package com.Saisnigdha.Design_Patterns.Creational_patterns.FactoryPattern;
+
+public class Circle implements Shape {
+
+	   @Override
+	   public void draw() {
+	      System.out.println("Inside Circle::draw() method.");
+	   }
+	}
---
+++ epam6-master/README.md.txt
@@ -0,0 +1,1 @@
+# saisnigdha_Design_Patterns
